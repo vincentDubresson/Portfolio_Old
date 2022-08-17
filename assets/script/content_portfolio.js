da36a565bf4ac74a99b607fe8ef85563f49629e5
vincentDubresson <vincent.dubresson@orange.fr>
2022-08-17 15:42:20 +0200

    Add the possibility to download the CV in portfolio part

--- assets/script/content_portfolio.js
+++ assets/script/content_portfolio.js
@@ -4,8 +4,12 @@ if (document.querySelectorAll('.portfolio-card-fr')) {
     const englishPortfolioCards = document.querySelectorAll('.portfolio-card-en');
     const frenchPortfolioFlag = document.querySelector('.portfolio-fr-flag-icon');
     const englishPortfolioFlag = document.querySelector('.portfolio-en-flag-icon');
+    const frenchCurriculum = document.querySelector('.portfolio-curriculum-fr');
+    const englishCurriculum = document.querySelector('.portfolio-curriculum-en');
 
     frenchPortfolioFlag.addEventListener('click', () => {
+        frenchCurriculum.classList.toggle('display-none');
+        englishCurriculum.classList.toggle('display-none');
         frenchPortfolioFlag.classList.toggle('display-none');
         englishPortfolioFlag.classList.toggle('display-none');
         for (let i = 0; i < frenchPortfolioCards.length; i++) {
@@ -15,6 +19,8 @@ if (document.querySelectorAll('.portfolio-card-fr')) {
     });
 
     englishPortfolioFlag.addEventListener('click', () => {
+        frenchCurriculum.classList.toggle('display-none');
+        englishCurriculum.classList.toggle('display-none');
         frenchPortfolioFlag.classList.toggle('display-none');
         englishPortfolioFlag.classList.toggle('display-none');
         for (let i = 0; i < englishPortfolioCards.length; i++) {
